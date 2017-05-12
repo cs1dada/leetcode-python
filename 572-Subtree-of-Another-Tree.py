995a8ebe1f8b8eded90e281eafe7325504539430
cs1dada <q2275357@hotmail.com>
2017-05-12 17:05:48 +0800

    572-Subtree-of-Another-Tree

--- 572-Subtree-of-Another-Tree.py
+++ 572-Subtree-of-Another-Tree.py
@@ -23,9 +23,42 @@ class Solution(object):
         :type t: TreeNode
         :rtype: bool
         """
+        if t is None:
+            return True
+        if s is None:
+            return False
 
+        if self.recurCheck(s, t):
+            return True
+        
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
 
+    def recurCheck(self, root1, root2):
+        if root1 is None and root2 is None:
+            return True
+        elif root1 is None or root2 is None:
+            return False
 
+        if root1.val == root2.val:
+            return self.recurCheck(root1.left, root2.left) and self.recurCheck(root1.right, root2.right)
+        else:
+            return False
+    '''
+     Tree T is a subtree of S if both inorder and preorder traversals of T are 
+     substrings of inorder and preorder traversals of S respectively.
+    '''
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(4)
+    root.right = TreeNode(5)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(2)
+    root.left.right.left = TreeNode(0)
+
+    subRoot = TreeNode(4)
+    subRoot.left = TreeNode(1)
+    subRoot.right = TreeNode(2)
+    print (Solution().isSubtree(root, subRoot))
 '''
 Given tree s:
 
